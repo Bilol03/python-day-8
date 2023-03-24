b8556cefc1cfb7c615aa5a055b5230c35a1bf706
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-03-24 15:41:17 +0900

    Learning python functions

---
+++ main.py
@@ -0,0 +1,16 @@
+def greet():
+    print("Hello")
+    print("How are you?")
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+
+greet_with_name("Bilol")
+
+
+def introducing(name, age):
+    print(f"Hello my name is {name} and I am {age} years old")
+
+introducing("Bilol", 19)
