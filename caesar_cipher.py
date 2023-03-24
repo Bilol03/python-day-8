48fb38a164dfd895676b6727402a688d9b066c51
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-03-24 17:46:37 +0900

    Second part has been finished

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text,shift):
-  res = ""
-  for i in text:
-    index = alphabet.index(i)
-    index += shift
-    res += alphabet[index]
-  print(res)
-encrypt(text=text, shift=shift)
+def encrypt(plain_text, plain_shift):
+    res = ""
+    for i in plain_text:
+        index = alphabet.index(i)
+        index += plain_shift
+        res += alphabet[index]
+    print(res)
+
+def decrypt(plain_text, plain_shift):
+    res = ""
+    for i in plain_text:
+        index = alphabet.index(i)
+        index -= plain_shift
+        res += alphabet[index]
+    print(res)
+
+if direction == "encode":
+    encrypt(plain_text=text, plain_shift=shift)
+else:
+    decrypt(plain_text=text, plain_shift=shift)
