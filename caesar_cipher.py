fdacd50984e0dbbb7790bd602476d0653be957c2
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-03-25 15:52:58 +0900

    Bugs are fixed and the program hass been finished

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,23 +1,46 @@
+from ceaser_art import logo
+print(logo)
+
+end_game = False
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-
-def ceasar(plain_text, plain_shift, direction):
+def caesar(plain_text, plain_shift, direction):
     res = ""
-
-    for i in plain_text:
-        index = alphabet.index(i)
-
-        if direction == "decode":
-            plain_shift *= -1
-        index += plain_shift
-        res += alphabet[index]
+    if direction == "decode":
         plain_shift *= -1
+
+    for char in plain_text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            index += plain_shift
+            res += alphabet[index]
+        else:
+            res += char
 
     print(f"Your {direction}d word is {res}")
 
-ceasar(direction=direction, plain_text=text, plain_shift=shift)
+
+while end_game != True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if not end_game:
+        caesar(plain_text=text, plain_shift=shift, direction=direction)
+        end_game_ask = input("Dou you want to finish this game? Yes or No: ")
+    if end_game_ask == "Yes" or end_game_ask == "yes":
+        end_game = True
+        print("Good Bye")
+        break
+    else:
+        continue
+
+while shift > 26:
+    if shift % 2 == 0:
+        shift = shift // 2
+    elif shift % 3 == 0:
+        shift = shift // 3
+    else:
+        shift = shift // 5
+
